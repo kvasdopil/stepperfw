37e5dc957a2ce155a124a050f9867233f06a276b
Alexey Guskov <alexey.guskov@ombori.com>
2023-10-20 18:01:24 +0200

    fix: support xy

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,22 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
 
-auto PULSE = D5;
-auto DIR = D6;
+auto PULSE1 = D5;
+auto DIR1 = D6;
 
-AccelStepper stepper1 = AccelStepper(1, PULSE, DIR);
+auto PULSE2 = D1;
+auto DIR2 = D0;
+
+AccelStepper stepperX = AccelStepper(1, PULSE1, DIR1);
+AccelStepper stepperY = AccelStepper(1, PULSE2, DIR2);
 
 void setup()
 {
-  stepper1.setMaxSpeed(1500);
-  stepper1.setAcceleration(500);
+  stepperX.setMaxSpeed(1500);
+  stepperX.setAcceleration(500);
+
+  stepperY.setMaxSpeed(1500);
+  stepperY.setAcceleration(500);
   // stepper1.moveTo(24);
 
   Serial.begin(115200);
@@ -43,51 +50,47 @@ const int REPORT_STATUS_EVERY_MS = 100;
 
 void loop()
 {
-  // // delay(1);
-  // delayMicroseconds(500);
-  // digitalWrite(D5, 0);
-  // // delay(1);
-  // delayMicroseconds(500);
-  // if (target != pos)
-  // {
-  //   digitalWrite(DIR, target > pos ? 0 : 1);
-  //   digitalWrite(D5, 1);
-  //   target > pos ? pos++ : pos--;
-  // }
-
-  // stepper1.moveTo(8000);
-  // stepper1.runToPosition();
-  // stepper1.moveTo(0);
-  // stepper1.runToPosition();
-
   String *line = readLine();
   if (line != NULL)
   {
-    float target;
-    Serial.println(String("Got command ") + line->c_str());
-    sscanf(line->c_str(), "%f", &target);
-
-    stepper1.moveTo(target * STEPS_PER_1_DEGREE);
+    if (line->length() > 0)
+    {
+      if (line->charAt(0) == String("X").charAt(0))
+      {
+        Serial.println(String("moving x: ") + line->substring(1));
+        auto tgt = line->substring(1).toFloat();
+        stepperX.moveTo(tgt * STEPS_PER_1_DEGREE);
+      }
+      if (line->charAt(0) == String("Y").charAt(0))
+      {
+        Serial.println(String("moving y: ") + line->substring(1));
+        auto tgt = line->substring(1).toFloat();
+        stepperY.moveTo(tgt * STEPS_PER_1_DEGREE);
+      }
+    }
   }
 
   // print stepper position every .5 seconds
   static unsigned long lastPrint = 0;
   static bool moving = false;
-  if (stepper1.distanceToGo() != 0)
+  if (stepperX.distanceToGo() != 0 || stepperY.distanceToGo() != 0)
   {
     moving = true;
     if (millis() - lastPrint > REPORT_STATUS_EVERY_MS)
     {
       lastPrint = millis();
-      Serial.println(stepper1.currentPosition() / STEPS_PER_1_DEGREE);
+      Serial.println(String("X:") + (stepperX.currentPosition() / STEPS_PER_1_DEGREE) + " " +
+                     String("Y:") + (stepperX.currentPosition() / STEPS_PER_1_DEGREE));
     }
   }
   else if (moving)
   {
     // print final position
     moving = false;
-    Serial.println(stepper1.currentPosition() / STEPS_PER_1_DEGREE);
+    Serial.println(String("X:") + (stepperX.currentPosition() / STEPS_PER_1_DEGREE) + " " +
+                   String("Y:") + (stepperX.currentPosition() / STEPS_PER_1_DEGREE));
   }
 
-  stepper1.run();
+  stepperX.run();
+  stepperY.run();
 }
