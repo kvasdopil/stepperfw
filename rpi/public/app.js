a9bc65c087aa12e46c4f37282b7a5b0f850f2171
Alexey Guskov <alexey.guskov@ombori.com>
2023-10-30 14:09:21 +0100

    fix: use smart servos on middle joints

--- rpi/public/app.js
+++ rpi/public/app.js
@@ -8,6 +8,21 @@ ws.onerror = () => {
   console.log('error');
 }
 
+const servo = async (msg) => {
+  try {
+    const res = await fetch('http://servos.local', {
+      method: 'POST',
+      body: JSON.stringify(msg),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return res.json();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
 const send = (msg) => {
   // console.log('>', msg);
   if (msg instanceof Array)
@@ -92,6 +107,28 @@ const stop = async (id) => {
   unlock();
 }
 
+const monitorServos = async (cb) => {
+  let prevx = null;
+  let prevy = null;
+  while (true) {
+    await delay(200);
+    try {
+      const res = await fetch('http://servos.local');
+      const { x, y } = await res.json();
+
+      if (x !== prevx || y !== prevy) {
+        // console.log('axis', id, angle);
+        cb(x / 40000 * 360, y / 40000 * 360);
+        prevx = x;
+        prevy = y;
+      }
+    } catch (e) {
+      console.error(e);
+      await delay(1000);
+    }
+  }
+}
+
 const monitorAxis = async (id, cb) => {
   let prev = null;
   while (true) {
@@ -147,10 +184,9 @@ const rotate = async (id, speed, position) => {
 }
 
 let lastX = null;
-const moveX = async (x) => {
+const moveW = async (x) => {
   const axis = 1;
   if (lastX === null) lastX = (await getPulses(axis)) / 80000 * 360;
-  console.log(x, lastX)
   await rotate(axis, 120, x - lastX);
   lastX = x;
 }
@@ -158,14 +194,23 @@ let lastY = null;
 const moveY = async (y) => {
   const axis = 0;
   if (lastY === null) lastY = (await getPulses(axis)) / 80000 * 360;
-  console.log(y, lastY)
   await rotate(axis, 120, y - lastY);
   lastY = y;
+}
+
+const moveX = async (w) => {
+  servo({ y: Math.round(w / 360 * 40000) });
+}
+
+const moveR = async (r) => {
+  servo({ x: Math.round(r / 360 * 40000) });
 }
 
 const App = () => {
   const [xPos, setXPos] = useState(0);
   const [yPos, setYPos] = useState(0);
+  const [rPos, setRPos] = useState(0);
+  const [wPos, setWPos] = useState(0);
   const [connected, setConnected] = useState(ws.readyState === 1);
 
   useEffect(() => {
@@ -183,9 +228,14 @@ const App = () => {
 
       if (await checkUartEnabled(1)) {
         monitorAxis(1, (angle) => {
-          setXPos(angle);
+          setWPos(angle);
         });
       }
+
+      monitorServos((r, x) => {
+        setXPos(x);
+        setRPos(r);
+      });
       // send("X?");
       // send("Y?");
     }
@@ -222,45 +272,11 @@ const App = () => {
 
   const [tgtX, setTgtX] = useState(null);
   const [tgtY, setTgtY] = useState(null);
-
-  const setRot = (e) => {
-    const { left, top, width, height } = e.target.getBoundingClientRect();
-    const dx = (e.clientX - left) / width - 0.5;
-    const dy = (e.clientY - top) / height - 0.5;
-    const angle = 90 + Math.atan2(dy, dx) * 180 / Math.PI;
-    setTgtX(angle);
-    return angle;
-  }
-
-  const setElev = (e) => {
-    const { left, top, width, height } = e.target.getBoundingClientRect();
-    const dy = (e.clientY - top) / height - 0.5;
-    setTgtY(190 * dy);
-    return 190 * dy;
-  }
-
-  const onCircleMove = (e) => {
-    if (e.buttons === 0) return;
-    setRot(e);
-  }
-
-
-  const onCircleClick = async (e) => {
-    const a = setRot(e);
-    await moveX(a);
-  }
-
-  const onRectMove = (e) => {
-    if (e.buttons === 0) return;
-    setElev(e);
-  }
-
-  const onRectClick = async (e) => {
-    const a = setElev(e);
-    await moveY(a);
-  }
+  const [tgtR, setTgtR] = useState(null);
+  const [tgtW, setTgtW] = useState(null);
 
   const stopClick = async () => {
+    fetch('http://servos.local/stop').catch(e => console.error(e));
     await stop(0);
     await stop(1);
     lastX = null;
@@ -270,36 +286,10 @@ const App = () => {
   return (
     <div>
       <div style={{ display: "flex" }}>
-        <svg width="300" height="300" viewBox="0 0 100 100" >
-          <circle cx="50" cy="50" r="49" fill="#333" stroke="#ccc" />
-          <g style={{ transformOrigin: '50% 50%', transform: `rotate(${xPos}deg)` }}>
-            <line x1="45" y1="10" x2="50" y2="1" stroke="#ccc" />
-            <line x1="55" y1="10" x2="50" y2="1" stroke="#ccc" />
-          </g>
-          {tgtX !== null && <g style={{ transformOrigin: '50% 50%', transform: `rotate(${tgtX}deg)` }}>
-            <line x1="45" y1="15" x2="50" y2="6" stroke="#0c3" />
-            <line x1="55" y1="15" x2="50" y2="6" stroke="#0c3" />
-          </g>}
-          {connected && tgtX !== null && <text x={50} y={50} fill="#0c3" style={{ textAnchor: 'middle', fontFamily: 'sans-serif', fontSize: 10, userSelect: 'none' }}>{tgtX.toFixed(2)}</text>}
-          {connected && <text x={50} y={60} fill="#ccc" style={{ textAnchor: 'middle', fontFamily: 'sans-serif', fontSize: 10, userSelect: 'none' }}>{xPos.toFixed(2)}</text>}
-          {connected && <rect x="0" y="0" width="100" height="100" fill="transparent" stroke="transparent" onClick={onCircleClick} onMouseMove={onCircleMove} />}
-          {!connected && <text x={50} y={55} fill="#f33" style={{ textAnchor: 'middle', fontFamily: 'sans-serif', fontSize: 10, userSelect: 'none' }}>NO CONNECTION</text>}
-        </svg>
-        <svg width="200" height="300" viewBox="0 0 60 100" >
-          <rect x="0" y="0" width="59" height="99" fill="#333" stroke="#ccc" />
-          {tgtY !== null && <g style={{ transformOrigin: '0% 50%', transform: `translateY(${tgtY * 0.52}px)` }}>
-            <line x1="4" y1="50" x2="9" y2="45" stroke="#0c3" />
-            <line x1="4" y1="50" x2="9" y2="55" stroke="#0c3" />
-          </g>}
-          <g style={{ transformOrigin: '0% 50%', transform: `translateY(${yPos * 0.52}px)` }}>
-            <line x1="1" y1="50" x2="6" y2="45" stroke="#ccc" />
-            <line x1="1" y1="50" x2="6" y2="55" stroke="#ccc" />
-          </g>
-          {connected && tgtY !== null && <text x={15} y={50} fill="#0c3" style={{ fontFamily: 'sans-serif', fontSize: 10, userSelect: 'none' }}>{tgtY.toFixed(2)}</text>}
-          {connected && <text x={15} y={60} fill="#ccc" style={{ fontFamily: 'sans-serif', fontSize: 10, userSelect: 'none' }}>{yPos.toFixed(2)}</text>}
-          {!connected && <text x={30} y={55} fill="#f33" style={{ width: 30, textAnchor: 'middle', fontFamily: 'sans-serif', fontSize: 10, userSelect: 'none' }}>NO CONN</text>}
-          {connected && <rect x="0" y="0" width="60" height="100" fill="transparent" stroke="transparent" onClick={onRectClick} onMouseMove={onRectMove} />}
-        </svg>
+        <GaugeRound connected={connected} target={tgtX} value={xPos} onChange={pos => setTgtX(pos)} onMove={pos => moveX(pos)} />
+        <GaugeSquare connected={connected} target={tgtY} value={yPos} onChange={pos => setTgtY(pos)} onMove={pos => moveY(pos)} />
+        <GaugeSquare connected={connected} target={tgtW} value={wPos} onChange={pos => setTgtW(pos)} onMove={pos => moveW(pos)} />
+        <GaugeRound connected={connected} target={tgtR} value={rPos} onChange={pos => setTgtR(pos)} onMove={pos => moveR(pos)} />
         <svg width="150" height="150" viewBox="0 0 100 100" style={{ paddingLeft: 8, cursor: 'pointer' }} onClick={stopClick}>
           <circle cx="50" cy="50" r="40" stroke="#f33" fill="#333" strokeWidth={5} />
           {connected && <text x={50} y={58} fill="#f33" style={{ fontFamily: 'sans-serif', textAnchor: 'middle', fontSize: 20, userSelect: 'none' }}>STOP</text>}
@@ -307,6 +297,79 @@ const App = () => {
       </div>
     </div>
   );
+};
+
+const GaugeRound = ({ connected, target, value, onChange, onMove }) => {
+  const click2degrees = (e) => {
+    const { left, top, width, height } = e.target.getBoundingClientRect();
+    const dx = (e.clientX - left) / width - 0.5;
+    const dy = (e.clientY - top) / height - 0.5;
+    return 90 + Math.atan2(dy, dx) * 180 / Math.PI;
+  }
+
+  const onCircleClick = async (e) => {
+    const a = click2degrees(e);
+    onChange(a);
+    onMove(a);
+  }
+
+  const onCircleMove = (e) => {
+    if (e.buttons === 0) return;
+    const a = click2degrees(e);
+    onChange(a);
+  }
+
+  return <svg width="300" height="300" viewBox="0 0 100 100" >
+    <circle cx="50" cy="50" r="49" fill="#333" stroke="#ccc" />
+    <g style={{ transformOrigin: '50% 50%', transform: `rotate(${value}deg)` }}>
+      <line x1="45" y1="10" x2="50" y2="1" stroke="#ccc" />
+      <line x1="55" y1="10" x2="50" y2="1" stroke="#ccc" />
+    </g>
+    {target !== null && <g style={{ transformOrigin: '50% 50%', transform: `rotate(${target}deg)` }}>
+      <line x1="45" y1="15" x2="50" y2="6" stroke="#0c3" />
+      <line x1="55" y1="15" x2="50" y2="6" stroke="#0c3" />
+    </g>}
+    {connected && target !== null && <text x={50} y={50} fill="#0c3" style={{ textAnchor: 'middle', fontFamily: 'sans-serif', fontSize: 10, userSelect: 'none' }}>{target.toFixed(2)}</text>}
+    {connected && <text x={50} y={60} fill="#ccc" style={{ textAnchor: 'middle', fontFamily: 'sans-serif', fontSize: 10, userSelect: 'none' }}>{value.toFixed(2)}</text>}
+    {connected && <rect x="0" y="0" width="100" height="100" fill="transparent" stroke="transparent" onClick={onCircleClick} onMouseMove={onCircleMove} />}
+    {!connected && <text x={50} y={55} fill="#f33" style={{ textAnchor: 'middle', fontFamily: 'sans-serif', fontSize: 10, userSelect: 'none' }}>NO CONNECTION</text>}
+  </svg>
+}
+
+const GaugeSquare = ({ connected, target, value, onChange, onMove }) => {
+  const event2degrees = (e) => {
+    const { top, height } = e.target.getBoundingClientRect();
+    const dy = (e.clientY - top) / height - 0.5;
+    return 190 * dy;
+  }
+
+  const onRectMove = (e) => {
+    if (e.buttons === 0) return;
+    const a = event2degrees(e);
+    onChange(a);
+  }
+
+  const onRectClick = async (e) => {
+    const a = event2degrees(e);
+    onChange(a);
+    onMove(a);
+  }
+
+  return <svg width="200" height="300" viewBox="0 0 60 100" >
+    <rect x="0" y="0" width="59" height="99" fill="#333" stroke="#ccc" />
+    {target !== null && <g style={{ transformOrigin: '0% 50%', transform: `translateY(${target * 0.52}px)` }}>
+      <line x1="4" y1="50" x2="9" y2="45" stroke="#0c3" />
+      <line x1="4" y1="50" x2="9" y2="55" stroke="#0c3" />
+    </g>}
+    <g style={{ transformOrigin: '0% 50%', transform: `translateY(${value * 0.52}px)` }}>
+      <line x1="1" y1="50" x2="6" y2="45" stroke="#ccc" />
+      <line x1="1" y1="50" x2="6" y2="55" stroke="#ccc" />
+    </g>
+    {connected && target !== null && <text x={15} y={50} fill="#0c3" style={{ fontFamily: 'sans-serif', fontSize: 10, userSelect: 'none' }}>{target.toFixed(2)}</text>}
+    {connected && <text x={15} y={60} fill="#ccc" style={{ fontFamily: 'sans-serif', fontSize: 10, userSelect: 'none' }}>{value.toFixed(2)}</text>}
+    {!connected && <text x={30} y={55} fill="#f33" style={{ width: 30, textAnchor: 'middle', fontFamily: 'sans-serif', fontSize: 10, userSelect: 'none' }}>NO CONN</text>}
+    {connected && <rect x="0" y="0" width="60" height="100" fill="transparent" stroke="transparent" onClick={onRectClick} onMouseMove={onRectMove} />}
+  </svg>
 };
 
 ReactDOM.render(
