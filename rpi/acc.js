f881042465fdb571f4248d9a8235de3d28ac0a30
Alexey Guskov <alexey.guskov@ombori.com>
2023-11-03 16:35:40 +0100

    fix: acc script

--- rpi/acc.js
+++ rpi/acc.js
@@ -10,59 +10,33 @@ const chunks = [];
 
 async function main() {
   const riface = await new Promise((resolve, reject) => bus.getInterface(service, READ_CHAR, "org.bluez.GattCharacteristic1", (err, iface) => err ? reject(err) : resolve(iface)));
-  // bus.getInterface(service, READ_CHAR, "org.bluez.GattCharacteristic1", (err, iface) => {
-  // if (err) {
-  //   console.error(`Error getting interface: ${err.message}`);
-  //   return;
-  // }
-  console.log('Iface ok');
   riface.StartNotify((e, v) => console.log('startn', e, v));
-  // });
 
-  bus.getInterface(service, READ_CHAR, 'org.freedesktop.DBus.Properties', (err, iface) => {
-    if (err) {
-      console.error(`Error getting interface: ${err.message}`);
-      return;
-    }
-    console.log(`Interface obtained`);
-
-    //  iface.StartNotify((e,v) => console.log('start notify', e || 'ok'));
-
-    iface.on('PropertiesChanged', (ifaceName, changedProps, invalidatedProps) => {
+  const notify = await new Promise((resolve, reject) => bus.getInterface(service, READ_CHAR, "org.freedesktop.DBus.Properties", (err, iface) => err ? reject(err) : resolve(iface)));
+  const result = new Promise(resolve => {
+    notify.on('PropertiesChanged', (ifaceName, changedProps, invalidatedProps) => {
       for (const cp of changedProps) {
         const [name, a] = cp;
-        if (name === 'Value') {
-          //     console.log(a,b)
-          const [info, data] = a;
-          const chunk = data[0].toString();
-          //console.log(data[0], chunk);
-          chunks.push(chunk);
-          if (!chunk.endsWith("\x0d\x0a\x3e")) return;
-          const line = chunks.join('').split("\n").map(a => a.trim());
-          const l = line.find(line => line.startsWith('='));
-          if (!l) return;
-          console.log(yaml.parse(l.substring(1)));
-          process.exit(0);
-        }
-      }
-      //    console.log('pc', changedProps);
-      //    if (changedProps.Value) {
-      //      const value = changedProps.Value;
-      //      console.log(`Value changed: ${value.toString('hex')}`);
-      //    }
-    });
+        if (name !== 'Value') return;
+        const [info, data] = a;
+        const chunk = data[0].toString();
 
-    bus.getInterface(service, WRITE_CHAR, "org.bluez.GattCharacteristic1", (err, iface) => {
-      if (err) {
-        console.error(`Error getting interface: ${err.message}`);
-        return;
+        chunks.push(chunk);
+        if (!chunk.endsWith("\x0d\x0a\x3e")) return;
+        const line = chunks.join('').split("\n").map(a => a.trim());
+        const l = line.find(line => line.startsWith('='));
+        if (!l) return;
+        resolve(l);
       }
-      console.log('Iface ok');
-      const res = iface.WriteValue(Buffer.from('acc.read();\r\n'), {}, (e, v) => console.log('w', e, v));
-
     });
   });
 
+  const wiface = await new Promise((resolve, reject) => bus.getInterface(service, WRITE_CHAR, "org.bluez.GattCharacteristic1", (err, iface) => err ? reject(err) : resolve(iface)));
+  await new Promise((resolve, reject) => wiface.WriteValue(Buffer.from('acc.read();\r\n'), {}, (e) => e ? reject(e) : resolve()));
+
+  const res = await result;
+  console.log(yaml.parse(res.substring(1)));
+  process.exit(0);
 }
 
 main();
